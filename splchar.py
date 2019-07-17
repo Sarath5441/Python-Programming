fbc03e36f5b1d84ecf7b85c92da02ad167661de8
Sarath5441 <51222749+Sarath5441@users.noreply.github.com>
2019-07-17 12:32:03 +0530

    Create splchar.py

---
+++ splchar.py
@@ -0,0 +1,8 @@
+u=input()
+g=0
+for word in u:
+  
+  if word=='.'or word=='@'or word=='%'or word=='&'or word=='*'or word=='6'or word=='#'or word=='!'or word=='$' or word=='_':
+    g+=1
+  
+print(g) 
